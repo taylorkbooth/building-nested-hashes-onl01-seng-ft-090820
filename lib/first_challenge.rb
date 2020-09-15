266faa234621bc9078cda30229ff737b0b22df48
Taylor Booth <taylorkbooth@gmail.com>
2020-09-15 18:16:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,12 +5,7 @@ def first_challenge
   
      },
      
-     :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
      
-       
      }
+  }
 end
