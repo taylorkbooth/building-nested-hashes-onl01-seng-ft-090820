91bc2846b8aa81a9bebfcc569323e4689f0ad50e
Taylor Booth <taylorkbooth@gmail.com>
2020-09-15 18:16:21 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,12 +2,7 @@ def first_challenge
    epic_tragedy = {
      :montague => {
        
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
-     
-       
+  
      },
      
      :capulet => {
