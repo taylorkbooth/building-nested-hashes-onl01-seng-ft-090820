c120efa21a714bce6042bb471ba96de89bd045b3
Taylor Booth <taylorkbooth@gmail.com>
2020-09-15 18:27:34 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,21 +1,19 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      
+   :montague => {
       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
-       
-      }, 
-    :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
-     
-       
-      }}
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
+ 
 
   
 end
